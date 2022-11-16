7b694333861ebd67731b20d502117c6fb4652d4c
Trinity DePaulis <trinity.depaulis@gmail.com>
2022-11-16 11:32:49 -0600

    my navigateByUrl isnt a function.

--- liquid-prep-app/src/app/components/my-crops/my-crops.component.spec.ts
+++ liquid-prep-app/src/app/components/my-crops/my-crops.component.spec.ts
@@ -1,26 +1,38 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-
 import { MyCropsComponent } from './my-crops.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http'
 
 describe('MyCropsComponent', () => {
-  let component: MyCropsComponent;
   let fixture: ComponentFixture<MyCropsComponent>;
+  let mockRouter = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ MyCropsComponent ]
+      declarations: [ MyCropsComponent],
+      imports: [HttpClientModule],
+      providers: [
+        { provide: Router, useValue: mockRouter}
+       ]
     })
-    .compileComponents();
+    .compileComponents();   
+    // fixture = TestBed.createComponent(MyCropsComponent);
+    // router = TestBed.inject(Router)
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MyCropsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should navigate', () => {
+    // const component = fixture.componentInstance;
+    // const navigateSpy = spyOn(router, "navigateByUrl");
+    let fixture = TestBed.createComponent(MyCropsComponent);
+    fixture.detectChanges();
+    let component: MyCropsComponent = fixture.componentInstance;
+
+    component.fabClicked();
+    expect(mockRouter.navigate).toHaveBeenCalledWith('/select-crop');
   });
 
   
