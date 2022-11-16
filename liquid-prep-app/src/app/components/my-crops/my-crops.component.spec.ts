ef1080be7aca567a370de2e5ca81968b363f8413
Trinity DePaulis <trinity.depaulis@gmail.com>
2022-11-16 13:01:49 -0600

    cannot read properties of undefined error

--- liquid-prep-app/src/app/components/my-crops/my-crops.component.spec.ts
+++ liquid-prep-app/src/app/components/my-crops/my-crops.component.spec.ts
@@ -2,27 +2,32 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { MyCropsComponent } from './my-crops.component';
 import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 
 describe('MyCropsComponent', () => {
+  let component: MyCropsComponent;
   let fixture: ComponentFixture<MyCropsComponent>;
   let mockRouter = {
     navigate: jasmine.createSpy('navigate')
   };
+  const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ MyCropsComponent],
       imports: [HttpClientModule],
       providers: [
-        { provide: Router, useValue: mockRouter}
+        { provide: Router, useValue: routerSpy}
        ]
     })
     .compileComponents();   
-    // fixture = TestBed.createComponent(MyCropsComponent);
+    fixture = TestBed.createComponent(MyCropsComponent);
+    component = fixture.componentInstance;
     // router = TestBed.inject(Router)
   }));
 
+  it('should have as title', () => {
+  });
 
   it('should navigate', () => {
     // const component = fixture.componentInstance;
@@ -30,11 +35,28 @@ describe('MyCropsComponent', () => {
     let fixture = TestBed.createComponent(MyCropsComponent);
     fixture.detectChanges();
     let component: MyCropsComponent = fixture.componentInstance;
-
     component.fabClicked();
-    expect(mockRouter.navigate).toHaveBeenCalledWith('/select-crop');
+    expect(routerSpy).toHaveBeenCalledWith(['/select-crop']);
   });
 
-  
+  it('tab my crops should navigate to my crops', () => {
+    // const component = fixture.componentInstance;
+    // const navigateSpy = spyOn(router, "navigateByUrl");
+    let fixture = TestBed.createComponent(MyCropsComponent);
+    fixture.detectChanges();
+    let component: MyCropsComponent = fixture.componentInstance;
+    component.tabClicked("My Crops");
+    expect(routerSpy).toHaveBeenCalledWith(['/my-crops']);
+  });
+
+  it('tab settings should navigate to settings', () => {
+    // const component = fixture.componentInstance;
+    // const navigateSpy = spyOn(router, "navigateByUrl");
+    let fixture = TestBed.createComponent(MyCropsComponent);
+    fixture.detectChanges();
+    let component: MyCropsComponent = fixture.componentInstance;
+    component.tabClicked("Settings");
+    expect(routerSpy).toHaveBeenCalledWith(['/settings']);
+  });
 
 });
