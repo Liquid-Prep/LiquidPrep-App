546b8a42eb03f73a9f37558680b1b2c5f0ada7d8
Kenobi Jedi Ricarte <kenobiricarte@gmail.com>
2023-10-10 05:45:22 +0800

    Whole Design Finish

--- liquid-prep-app/src/app/components/fields-landing-page/details/details.component.ts
+++ liquid-prep-app/src/app/components/fields-landing-page/details/details.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { Location } from '@angular/common';
 import { HeaderService } from 'src/app/service/header.service';
 import { HeaderConfig } from 'src/app/models/HeaderConfig.interface';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-details',
@@ -14,12 +15,17 @@ export class DetailsComponent implements OnInit {
     leftIconName: 'close',
     rightIconName: 'delete',
     leftBtnClick: this.handleLeftClick.bind(this),
-    rightBtnClick: null,
+    rightBtnClick: this.openWarningDialog.bind(this),
   };
+
+  @ViewChild('warningDialog') dialogTemplate!: TemplateRef<any>;
+
+  private id = 'sadsadasd';
 
   constructor(
     private headerService: HeaderService,
     private location: Location,
+    private dialog: MatDialog,
   ) {}
 
   ngOnInit(): void {
@@ -30,7 +36,16 @@ export class DetailsComponent implements OnInit {
     this.backToMyFields();
   }
 
-  backToMyFields() {
+  public openWarningDialog() {
+    console.log('Delete');
+    this.dialog.open(this.dialogTemplate);
+  }
+
+  public deleteField() {
+    console.log('Deleted');
+  }
+
+  public backToMyFields() {
     this.location.back();
   }
 }
