e4cf22c5c49698d5d90fc1ac44776da2afbb4775
Crystal Reyes <crystalgardner@users.noreply.github.com>
2023-10-24 21:16:14 -0400

    Sensors landing page updates (#52)
    * Design change requests
    
    * fix sorted by display bug
    
    * Design change requests
    
    * fix sorted by display bug

--- liquid-prep-app/src/app/components/sort-modal/sort-modal.component.ts
+++ liquid-prep-app/src/app/components/sort-modal/sort-modal.component.ts
@@ -10,12 +10,14 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 export class SortModalComponent implements OnInit {
   title: string;
   selectedSortOption: string = 'lastUpdated';
+  selectedSortLabel: string = 'Last Updated';
 
   constructor(
     public dialogRef: MatDialogRef<SortModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
     this.selectedSortOption = data.selectedSortOption || 'lastUpdated';
+    this.selectedSortLabel = this.getLabelForSortOption(this.selectedSortOption);
   }
 
   ngOnInit(): void {}
@@ -25,6 +27,25 @@ export class SortModalComponent implements OnInit {
   }
 
   saveSelection() {
-    this.dialogRef.close(this.selectedSortOption);
+    const selection = {
+      selectedSortOption: this.selectedSortOption,
+      selectedSortLabel: this.getLabelForSortOption(this.selectedSortOption)
+    };
+    this.dialogRef.close(selection);
+  }
+
+  getLabelForSortOption(option: string): string {
+    switch (option) {
+      case 'lastUpdated':
+        return 'Last Updated Time';
+      case 'sensorName':
+        return 'Sensor Name (A-Z)';
+      case 'fieldLocation':
+        return 'Field Location (A-Z)';
+      case 'connectionStatus':
+        return 'Connection Status';
+      default:
+        return '';
+    }
   }
 }
