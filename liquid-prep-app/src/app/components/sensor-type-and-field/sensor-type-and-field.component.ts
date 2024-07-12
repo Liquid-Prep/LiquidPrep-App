b50de7029ea5f5c5c4e8ee474f49c12edbefd530
MICHAEL ANGELO DESTOR <destorm@ph.ibm.com>
2024-07-12 10:21:47 +0800

    Move sensor type and field id from sensor name to local storage

---
+++ liquid-prep-app/src/app/components/sensor-type-and-field/sensor-type-and-field.component.ts
@@ -0,0 +1,49 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { DialogComponent, DialogData } from '../dialog/dialog.component';
+import { FieldDataService } from 'src/app/service/FieldDataService';
+import { SensorStorageService } from 'src/app/service/sensor-storage.service';
+
+@Component({
+  selector: 'app-sensor-type-and-field',
+  templateUrl: './sensor-type-and-field.component.html',
+  styleUrls: ['./sensor-type-and-field.component.scss']
+})
+export class SensorTypeAndFieldComponent implements OnInit {
+
+  sensorType = '';
+  fieldId = '';
+
+  fieldOptions = [];
+  
+  constructor(
+    public dialogRef: MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private fieldService: FieldDataService,
+    private sensorStorageService: SensorStorageService
+  ) { }
+
+  async ngOnInit() {
+    this.sensorType = this.data?.sensor?.sensorType || '';
+    this.fieldId = this.data?.sensor?.fieldId || '';
+
+    let fields = await this.fieldService.getLocalStorageMyFields();
+    this.fieldOptions = fields.map(field=> {
+      return {
+        value: field.id,
+        label: field.fieldName
+      }
+    });
+  }
+
+  cancel(): void {
+    this.dialogRef.close();
+  }
+
+  save(): void {
+    if (this.data.mac) {
+      this.sensorStorageService.updateSensorByMacAddress(this.data.mac, this.sensorType, this.fieldId);
+      this.dialogRef.close("SAVED");
+    }
+  }
+}
--- liquid-prep-app/src/app/service/sensor-v2.service.ts
+++ liquid-prep-app/src/app/service/sensor-v2.service.ts
@@ -5,6 +5,7 @@ import { map, tap } from 'rxjs/operators';
 import { of } from 'rxjs';
 import { FieldDataService } from './FieldDataService';
 import { DecimalPipe } from '@angular/common';
+import { SensorStorageService } from './sensor-storage.service';
 
 @Injectable({
   providedIn: 'root',
@@ -14,7 +15,8 @@ export class SensorV2Service {
     private http: HttpClient,
     private webSocketService: WebSocketService,
     private fieldService: FieldDataService,
-    private decimalPipe: DecimalPipe
+    private decimalPipe: DecimalPipe,
+    private sensorStorageService: SensorStorageService
   ) {}
 
   sensorTypeMapper = {
@@ -24,33 +26,33 @@ export class SensorV2Service {
 
   fetchSensors() {
     let servers = this.webSocketService.getServers();
-    // return of({}).pipe(
-      return this.http.get<any>(`${servers.edgeGateway}/log`).pipe(
-      // map((response) => {
-      //   return {
-      //     status: 200,
-      //     timeSeries: {
-      //       B0A732818BC0: {
-      //         name: 'sensor_2-gen-GxfS',
-      //         id: 5,
-      //         moisture: '0.00',
-      //         timestamp: 1717408435799,
-      //       },
-      //       B0B21CA74064: {
-      //         name: 'GATEWAY-plm-i6AG',
-      //         id: 0,
-      //         moisture: 11.42,
-      //         timestamp: 1717408437896,
-      //       },
-      //       B0A73281D49C: {
-      //         name: 'sensor_1',
-      //         id: 5,
-      //         moisture: '0.00',
-      //         timestamp: 1717408436684,
-      //       },
-      //     },
-      //   };
-      // }),
+    return of({}).pipe(
+      // return this.http.get<any>(`${servers.edgeGateway}/log`).pipe(
+      map((response) => {
+        return {
+          status: 200,
+          timeSeries: {
+            B0A732818BC0: {
+              name: 'sensor_2-gen-GxfS',
+              id: 5,
+              moisture: '0.00',
+              timestamp: 1717408435799,
+            },
+            B0B21CA74064: {
+              name: 'GATEWAY-plm-i6AG',
+              id: 0,
+              moisture: 11.42,
+              timestamp: 1717408437896,
+            },
+            B0A73281D49C: {
+              name: 'sensor_1',
+              id: 5,
+              moisture: '0.00',
+              timestamp: 1717408436684,
+            },
+          },
+        };
+      }),
       tap((response) => {
         let oldData = this.webSocketService.getSensorData() || {};
         let timeSeries = Object.assign(oldData, response.timeSeries);
@@ -59,14 +61,15 @@ export class SensorV2Service {
       map((response) => {
         let devices = response.timeSeries;
         let data = [];
+        let sensorMap = this.sensorStorageService.getSensors();
         Object.keys(devices).forEach((key) => {
           let sensor = devices[key];
           sensor.mac = key;
           let fullName = sensor.name;
           let fullNameArr = fullName.split('-');
           let name = fullNameArr[0] || '';
-          let sensorType = fullNameArr[1] || '';
-          let fieldId = fullNameArr[2] || '';
+          let sensorType = sensorMap[key]?.sensorType || '';
+          let fieldId =  sensorMap[key]?.fieldId || '';
           let field = this.fieldService.getFieldFromMyFieldById(fieldId);
           let sensorTypeName = this.sensorTypeMapper[sensorType];
           let moisture = this.calibrateMoisture(sensor.moisture, sensorType, field?.soil);
